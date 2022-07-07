bb528363e506e51fea3ee7adc96a9a77535a97bb
terwer <youweics@163.com>
2022-07-07 18:52:28 +0800

    feat:token test

--- pages/api/public.js
+++ pages/api/public.js
@@ -1,23 +1,25 @@
-const fs = require('fs');
-const path = require("path")
+import fetch from 'node-fetch';
 
 export default function handler(req, res) {
     const imagePath = req.query.pic.replace("/public/", "")
-    const filePath = path.resolve(".", `public/${imagePath}`);
-    console.log("filePath=>", filePath)
-    // 已经同步直接取vercel的地址
-    // https://img1.terwergreen.com/api/public?pic=20220707172601.png
-    // http://localhost:3000/api/public?pic=20220707172601.png
-    if (fs.existsSync(filePath)) {
-        const absUrl = '/' + imagePath;
-        console.log("absUrl=>", absUrl)
-        res.redirect(307, '/' + imagePath).end()
-    } else {
-        // 获取中间代理地址
-        // https://img1.terwergreen.com/api/public/20220706194731.png
-        // http://localhost:3000/api/public/img?pic=20220706194731.png
-        const newUrl = 'https://ghproxy.com/https://raw.githubusercontent.com/terwer/upload/main/public/' + imagePath
-        console.log("newUrl=>", newUrl)
-        res.redirect(307, newUrl).end()
-    }
+    const fileUrl = `https://img1.terwergreen.com/${imagePath}`;
+    console.log("fileUrl=>", fileUrl)
+
+    fetch(fileUrl).then(async (v) => {
+        if (v.status == 200) {
+            const absUrl = '/' + imagePath;
+            console.log("absUrl=>", absUrl)
+            res.redirect(307, '/' + imagePath).end()
+        } else {
+            // 获取中间代理地址
+            // https://img1.terwergreen.com/api/public/20220706194731.png
+            // http://localhost:3000/api/public/img?pic=20220706194731.png
+            const newUrl = 'https://ghproxy.com/https://raw.githubusercontent.com/terwer/upload/main/public/' + imagePath
+            console.log("newUrl=>", newUrl)
+            res.redirect(307, newUrl).end()
+        }
+    }).catch(err => {
+        console.log(err)
+        res.end("500")
+    });
 }
