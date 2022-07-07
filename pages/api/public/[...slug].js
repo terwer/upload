a41e6a38a7c6676dd3fab7d0a35ef0cdeaeed71f
terwer <youweics@163.com>
2022-07-07 19:13:42 +0800

    feat:token test

--- pages/api/public/[...slug].js
+++ pages/api/public/[...slug].js
@@ -1,8 +1,15 @@
 import fetch from 'node-fetch';
 
 export default function handler(req, res) {
-    const imagePath = req.query.pic.replace("/public/", "")
-    const fileUrl = `https://img1.terwergreen.com/${imagePath}`;
+    // https://img1.terwergreen.com/api/public/20220706194731.png
+    const imagePath = req.query.slug.join("/");
+    let host = req.headers.host;
+    if (host.indexOf("localhost") > -1) {
+        host = "http://localhost:3000";
+    } else {
+        host = "https://img1.terwergreen.com";
+    }
+    const fileUrl = `${host}/${imagePath}`;
     console.log("fileUrl=>", fileUrl)
 
     fetch(fileUrl).then(async (v) => {
@@ -12,8 +19,6 @@ export default function handler(req, res) {
             res.redirect(307, '/' + imagePath).end()
         } else {
             // 获取中间代理地址
-            // https://img1.terwergreen.com/api/public/20220706194731.png
-            // http://localhost:3000/api/public/img?pic=20220706194731.png
             const newUrl = 'https://ghproxy.com/https://raw.githubusercontent.com/terwer/upload/main/public/' + imagePath
             console.log("newUrl=>", newUrl)
             res.redirect(307, newUrl).end()
