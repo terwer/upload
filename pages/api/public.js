169ba3ae39925f44a39022b4ba1d317209fd8c1a
terwer <youweics@163.com>
2022-07-07 18:39:07 +0800

    feat:token test

--- pages/api/public.js
+++ pages/api/public.js
@@ -2,11 +2,12 @@ const fs = require('fs');
 const path = require("path")
 
 export default function handler(req, res) {
-    const imagePath = req.query.slug.join("/")
+    const imagePath = req.query.pic.replace("/public/", "")
     const filePath = path.resolve(".", `public/${imagePath}`);
     console.log("filePath=>", filePath)
     // 已经同步直接取vercel的地址
-    // https://img1.terwergreen.com/api/public/20220707172601.png
+    // https://img1.terwergreen.com/api/public?pic=20220707172601.png
+    // http://localhost:3000/api/public?pic=20220707172601.png
     if (fs.existsSync(filePath)) {
         const absUrl = '/' + imagePath;
         console.log("absUrl=>", absUrl)
@@ -14,6 +15,7 @@ export default function handler(req, res) {
     } else {
         // 获取中间代理地址
         // https://img1.terwergreen.com/api/public/20220706194731.png
+        // http://localhost:3000/api/public/img?pic=20220706194731.png
         const newUrl = 'https://ghproxy.com/https://raw.githubusercontent.com/terwer/upload/main/public/' + imagePath
         console.log("newUrl=>", newUrl)
         res.redirect(307, newUrl).end()
